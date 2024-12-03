485c4a446d0d870307b537fdd5820386ebc7d96c
DestinyGerald <geralddestiny7@gmail.com>
2024-12-03 03:18:57 +0100

    first change

--- src/pages/Main/Advantages.jsx
+++ src/pages/Main/Advantages.jsx
@@ -1,22 +1,25 @@
 import './style.css'
 import './style.mobile.css'
 import { useState } from 'react'
+import { LuTarget } from 'react-icons/lu'
+import { FaMoneyBill } from 'react-icons/fa'
+import { IoColorPaletteSharp } from 'react-icons/io5'
 
 const AdvantagesCardInfo = [
 	{
-		icon: '',
+		icon: <LuTarget />,
 		title: 'Accuracy and Consistency',
 		little_content: 'We Provide Accurate designs that suits each clients demand with no stones left unturned.',
 		content: ''
 	},
 	{
-		icon: '',
+		icon: <FaMoneyBill />,
 		title: 'Cost Saving',
 		little_content: 'Get Satisfactory designs at reasonable and negotiatable prices. We consider every clients budget.',
 		content: ''
 	},
 	{
-		icon: '',
+		icon: <IoColorPaletteSharp />,
 		title: 'Personalization',
 		little_content: 'Our creatives work alongside clients request, considering each and every of a client\'s idea. ',
 		content: ''
@@ -38,7 +41,7 @@ function AdvantagesCard ({ icon, title, little_content, content }) {
 	return (
 		<div className='advantages-cnt-card' style={{ transform: `rotateY(${ deg * 180 }deg)` }} onClick={handleClick}>
 			<div className='advantages-cnt-card-front'>
-				<span>{icon}</span>
+				<div>{icon}</div>
 				<span>{title}</span>
 				<span>{little_content}</span>
 			</div>
